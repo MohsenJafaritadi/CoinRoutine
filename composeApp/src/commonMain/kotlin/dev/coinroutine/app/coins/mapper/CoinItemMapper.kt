0b85130d29b4de3afb4e99a7255794bd8799638d
dev_mohsenJafari <dev.mohsenjafaritadi@gmail.com>
2025-07-25 12:06:22 +0330

    Add GetCoinPriceHistoryUseCase

--- composeApp/src/commonMain/kotlin/dev/coinroutine/app/coins/mapper/CoinItemMapper.kt
+++ composeApp/src/commonMain/kotlin/dev/coinroutine/app/coins/mapper/CoinItemMapper.kt
@@ -1,7 +1,9 @@
 package dev.coinroutine.app.coins.mapper
 
 import dev.coinroutine.app.coins.data.remote.dto.CoinItemDto
+import dev.coinroutine.app.coins.data.remote.dto.CoinPriceDto
 import dev.coinroutine.app.coins.model.CoinModel
+import dev.coinroutine.app.coins.model.PriceModel
 import dev.coinroutine.app.core.domain.Coin
 
 
@@ -14,4 +16,9 @@ fun CoinItemDto.toCoinModel() = CoinModel(
     ),
     price = price,
     change = change,
+)
+
+fun CoinPriceDto.toPriceModel()= PriceModel(
+    price = price?:0.0,
+    timestamp=timestamp,
 )
